9e014dfb5ed931f94d264d94ee18eed29b8a6899
Erlon Zefanias Muhate <133228881+ErlonZefaniasMuhate@users.noreply.github.com>
2023-05-17 12:46:49 +0200

    Update main

--- Student Management System/src/projectPOO/Main.java
+++ Student Management System/src/projectPOO/Main.java
@@ -1,15 +1,22 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package projectPOO;
-import java.io.*;
+
+import java.time.LocalDateTime;
+
 /**
- *
+ * @author Erlon Z. Muhate
  * @author Leuso Nguenha
  */
 public class Main {
-    public static void main(String[] args) throws IOException{
-        
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(LocalDateTime.now());
     }
+    
 }
