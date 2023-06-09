11c8b0130bae4d978fca19ac3db445aca011e405
Erlon Zefanias Muhate <133228881+ErlonZefaniasMuhate@users.noreply.github.com>
2023-06-09 09:27:45 +0200

    Adicionaada funcionaldade de busca por nome, alterados nomes de variáveis para maior representatividade e adicionada documentação essencial à classe.

--- Student Management System/src/projectPOO/SystemUtils.java
+++ Student Management System/src/projectPOO/SystemUtils.java
@@ -6,6 +6,8 @@ package projectPOO;
 
 import erlon.utils.Validate;
 import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.nio.file.*;
 import java.util.*;
 import java.util.logging.*;
@@ -14,64 +16,124 @@ import java.util.logging.*;
  *
  * @author Erlon Z. Muhate
  * @param <Usuario> represents the user of the system
+ * @param <Entity> the type of entity, which can represent classrooms, departments, classes, assessments, disciplines, or other components of the system that are not users (they do not extend the User class) or activity logs
  */
-public abstract class SystemUtils<Usuario extends User & Serializable> {
+public abstract class SystemUtils<Usuario extends User & Serializable, Entity extends Serializable> {
 
-    private final String projectPath;
-    private final Path directoryPath;
+    private final String currentProjectPath;
+    private final Path filesDirectoryPath;
 
+    /**
+     * Construtor da classe SystemUtils. 
+     * Inicializa o caminho do diretório do projeto e o caminho do diretório de arquivos. 
+     * Se o diretório de arquivos não existe, ele é criado. 
+     * Caso ocorra algum erro ao criar o diretório, uma mensagem de erro é exibida e o programa é encerrado.
+     */
     public SystemUtils() {
-        projectPath = System.getProperty("user.dir");
-        directoryPath = Paths.get(projectPath, ".files");
-        if (!Files.exists(directoryPath)) {
+        // Obter o caminho do diretório atual do projeto
+        currentProjectPath = System.getProperty("user.dir");
+
+        // Definir o caminho para o diretório de arquivos
+        filesDirectoryPath = Paths.get(currentProjectPath, ".files");
+
+        // Verificar se o diretório de arquivos não existe
+        if (!Files.exists(filesDirectoryPath)) {
             try {
-                Files.createDirectories(directoryPath);
+                // Criar o diretório de arquivos
+                Files.createDirectories(filesDirectoryPath);
             } catch (IOException e) {
+                // Lidar com a exceção em caso de falha na criação do diretório
                 Logger.getLogger(SystemUtils.class.getName()).log(Level.SEVERE, "Failed to create the directory", e);
             } finally {
+                // Exibir uma mensagem de erro e encerrar o programa
                 System.out.println("Please, re-run the program!");
                 System.exit(1);
             }
         }
     }
-
+    
     /**
-     * @param code as the institutional user code
-     * @param fileName as the type of user to be found
+     * @param userCode as the institutional user code
+     * @param userType as the type of user to be found
      * @return User object
      * @throws NoSuchElementException if no element is found
      */
-    public Usuario getUserByCode(int code, String fileName) throws NoSuchElementException {
+    public Usuario findUserByCode(int userCode, String userType) throws NoSuchElementException {
         try {
-            var usersFromFile = readFromFile(fileName);
-            if (usersFromFile.containsKey(code)) {
-                return usersFromFile.get(code);
+            String fileName = userType + ".ser";
+            var existingUsers = loadUsersFromFile(fileName);
+            if (existingUsers.containsKey(userCode)) {
+                return existingUsers.get(userCode);
             } else {
                 throw new NoSuchElementException("A chave fornecida não corresponde a nenhum usuário");
             }
-        }catch(NoSuchFileException | FileNotFoundException e){
+        } catch (NoSuchFileException | FileNotFoundException e) {
             System.out.println("Desculpe, as informações solicitadas ainda não estão disponíveis.");
             System.exit(0);
         }
         return null;
     }
+    
+    /**
+    * Procura uma entidade pelo nome em um arquivo específico e retorna a primeira ocorrência encontrada.
+    *
+    * @param entityType o tipo da entidade (nome da classe) a ser procurada
+    * @param entityName o nome da entidade a ser procurada
+    * @return a entidade encontrada ou null se não for encontrada
+    */
+    public Entity findEntityByName(String entityType, String entityName) {
+        String fileName = entityType + ".ser";
+        try {
+            var existingEntities = loadEntitiesFromFile(fileName);
+            for (Entity e : existingEntities) {
+                try {
+                    Class<?> classe = Class.forName(entityType);
+                    if (classe.isInstance(e)) {
+                        // Fazendo o casting do objeto para a classe desejada
+                        Object objetoCasteado = classe.cast(e);
 
-    public Curso getCourseByName() {
+                        // Obtendo o método "getNome" na classe desejada
+                        Method getNomeMethod = classe.getMethod("getNome");
+
+                        // Invocando o método "getNome" no objeto casteado
+                        String nome = (String) getNomeMethod.invoke(objetoCasteado);
+
+                        if(nome.equals(entityName)){
+                            return (Entity)objetoCasteado;
+                        }
+                    }
+                } catch (ClassNotFoundException ex) {
+                    System.out.println("Classe não encontrada: " + entityType);
+                } catch (NoSuchMethodException ex) {
+                    System.out.println("Método getNome não encontrado na classe: " + entityType);
+                } catch (IllegalAccessException | InvocationTargetException ex) {
+                    System.out.println("Erro ao invocar o método getNome na classe: " + entityType);
+                }
+            }
+        } catch (NoSuchFileException e) {
+            e.getMessage();
+        } catch (IOException e) {
+            //handle the exception
+        }
         return null;
     }
 
-    @SuppressWarnings("InfiniteRecursion")
-    public void saveToFile(Usuario u, String fileName) {
-        if(!fileName.contains(".ser")){
-            fileName += ".ser";
-        }
-        Path filePath = directoryPath.resolve(fileName);
+    /**
+     * Salva um usuário em um arquivo específico, de acordo com o tipo de usuário.
+     *
+     * @param u o usuário a ser salvo
+     * @param userType o tipo de usuário
+     */
+    public void saveUserToFile(Usuario u, String userType) {
+
+        String fileName = userType + ".ser";
+        Path filePath = filesDirectoryPath.resolve(fileName);
 
         try ( ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(filePath, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING))) {
-            var usersFromFile = readFromFile(fileName);
-            if (!usersFromFile.containsKey(u.getCodigoInstituicional())) {
-                usersFromFile.put(u.getCodigoInstituicional(), u);
-                oos.writeObject(usersFromFile);
+            var existingUsers = loadUsersFromFile(fileName);
+            if (!existingUsers.containsKey(u.getCodigoInstituicional())) {
+                existingUsers.put(u.getCodigoInstituicional(), u);
+                oos.writeObject(existingUsers);
                 oos.flush();
             }
         } catch (IOException e) {
@@ -79,8 +141,16 @@ public abstract class SystemUtils<Usuario extends User & Serializable> {
         }
     }
 
-    private HashMap<Integer, Usuario> readFromFile(String fileName) throws NoSuchFileException, FileNotFoundException {
-        Path filePath = directoryPath.resolve(fileName);
+    /**
+     * Carrega os usuários de um arquivo de especificado.
+     *
+     * @param fileName o nome do arquivo a ser carregado
+     * @return um HashMap contendo os usuários carregados do arquivo
+     * @throws NoSuchFileException se o arquivo não for encontrado
+     * @throws FileNotFoundException se o arquivo não for encontrado
+     */
+    private HashMap<Integer, Usuario> loadUsersFromFile(String fileName) throws NoSuchFileException, FileNotFoundException {
+        Path filePath = filesDirectoryPath.resolve(fileName);
 
         try ( ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(filePath, StandardOpenOption.READ))) {
 
@@ -96,58 +166,84 @@ public abstract class SystemUtils<Usuario extends User & Serializable> {
         }
         return new HashMap<>();
     }
+    
+    /**
+    * Salva uma entidade em um arquivo.
+    *
+    * @param entity     a entidade a ser salva
+    * @param entityName o nome da entidade
+    */
+    public void saveEntityToFile(Entity entity, String entityName) {
+        String fileName = entityName + ".ser";
+        Path filePath = filesDirectoryPath.resolve(fileName);
 
-    //implementar metodos para ler e salvar listas de entidades(turmas, departamentos, entre outros), dessas listas extrair objectos e retornar ao sistema
-    /*public Curso findCourse(String fileName) {
-        Path filePath = directoryPath.resolve(fileName);
-
-        try ( ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(filePath, StandardOpenOption.READ))) {
-            var coursesFound = (List) ois.readObject();
-            ois.close();
-            
-        } catch (NoSuchFileException | FileNotFoundException e) {
-
-            try {
-                Files.createFile(filePath);
-            } catch (IOException ex) {
-                Logger.getLogger(SystemUtils.class.getName()).log(Level.SEVERE, "Excepcao encontrada: ", ex);
+        try ( ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(filePath, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING))) {
+            var existingEntities = loadEntitiesFromFile(fileName);
+            existingEntities.add(entity);
+            if (!existingEntities.contains(entity)) {
+                oos.writeObject(existingEntities);
+                oos.flush();
             }
-            return null;
+        } catch (IOException e) {
+            //handle the exception
+        }
+    }
+    
+    /**
+    * Carrega uma lista de entidades a partir de um arquivo.
+    *
+    * @param fileName o nome do arquivo contendo as entidades
+    * @return a lista de entidades carregadas
+    * @throws NoSuchFileException     se o arquivo não existe
+    * @throws FileNotFoundException se o arquivo não foi encontrado
+    */
+    private List<Entity> loadEntitiesFromFile(String fileName) throws NoSuchFileException, FileNotFoundException {
 
-        } catch (EOFException e) {
-            //reached end of file
-        } catch (StreamCorruptedException e) {
-            Logger.getLogger(SystemUtils.class.getName()).log(Level.SEVERE, " Ficheiro corrompido ", e);
+        Path filePath = filesDirectoryPath.resolve(fileName);
+        try ( ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(filePath, StandardOpenOption.READ))) {
+            List<Entity> entity = (List) ois.readObject();
+            ois.close();
+            return entity;
+        } catch (NoSuchFileException | FileNotFoundException e) {
+            throw e;
         } catch (IOException | ClassNotFoundException e) {
-            Logger.getLogger(SystemUtils.class.getName()).log(Level.SEVERE, " Excepcao encontrada ", e);
+            //handle the exception
         }
         return null;
     }
-    public void saveCourse(String fileName){
-        Path filePath = directoryPath.resolve(fileName);
-        try(ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(filePath, StandardOpenOption.TRUNCATE_EXISTING))){
-           var courses = findCourses
-        }catch(IOException e){
-            
-        }
-    }*/
-    public void saveLogin(Integer code, String password){
-        Path filePath = directoryPath.resolve("login.txt");
-        try(BufferedWriter writer = Files.newBufferedWriter(filePath, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.APPEND)){
-            writer.write(code+"||"+password);
+    
+    /**
+    * Salva as informações de login em um arquivo de texto.
+    *
+    * @param userCode   o código institucional do usuário
+    * @param password   a senha do usuário
+    * @param userType   o tipo de usuário
+    */
+    public void saveLoginInfo(Integer userCode, String password, String userType) {
+        Path filePath = filesDirectoryPath.resolve("login.txt");
+        try ( BufferedWriter writer = Files.newBufferedWriter(filePath, StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.APPEND)) {
+            writer.write(userCode + "||" + password);
             writer.newLine();
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    public void login() throws SecurityException, NoSuchFileException {
-        Path filePath = directoryPath.resolve("login.txt");
+    
+    /**
+    * Realiza o processo de login do usuário.
+    *
+    * @return o objeto User correspondente ao usuário logado
+    * @throws SecurityException     se as credenciais de login forem inválidas
+    * @throws NoSuchFileException   se os dados de login não estiverem disponíveis
+    */
+    public User login() throws SecurityException, NoSuchFileException {
+        Path filePath = filesDirectoryPath.resolve("login.txt");
 
         if (!Files.exists(filePath)) {
-            throw new NoSuchFileException("");
+            throw new NoSuchFileException("Login data is not available");
         }
 
-        try (BufferedReader reader = Files.newBufferedReader(filePath)) {
+        try ( BufferedReader reader = Files.newBufferedReader(filePath)) {
             System.out.println("Enter your institutional code: ");
             String code = reader.readLine();
             while (!Validate.isInteger(code)) {
@@ -162,17 +258,31 @@ public abstract class SystemUtils<Usuario extends User & Serializable> {
                 password = reader.readLine();
             }
 
-            boolean welcomeMessagePrinted = authenticateUser(code, password, reader);
+            boolean welcomeMessagePrinted = validateUserCredentials(code, password, reader);
 
             if (!welcomeMessagePrinted) {
                 reader.close();
                 throw new SecurityException("Access denied");
+            } else {
+                String userType = getUserType(Integer.parseInt(code), password) + ".ser";
+                return findUserByCode(Integer.parseInt(code), userType);
             }
         } catch (IOException e) {
+            //handle the exception
         }
+        return null;
     }
-
-    private boolean authenticateUser(String code, String password, BufferedReader reader) throws IOException {
+    
+    /**
+    * Valida as credenciais de um usuário.
+    *
+    * @param code     o código institucional do usuário
+    * @param password a senha do usuário
+    * @param reader   o BufferedReader para leitura dos dados de login
+    * @return true se as credenciais forem válidas e uma mensagem de boas-vindas for exibida, caso contrário, false
+    * @throws IOException se ocorrer um erro durante a leitura dos dados de login
+    */
+    private boolean validateUserCredentials(String code, String password, BufferedReader reader) throws IOException {
         String line;
         boolean welcomeMessagePrinted = false;
 
@@ -191,5 +301,32 @@ public abstract class SystemUtils<Usuario extends User & Serializable> {
             throw e;
         }
         return welcomeMessagePrinted;
+    }
+    
+    /**
+    * Obtém o tipo de usuário com base no código institucional e senha fornecidos.
+    *
+    * @param codigoInstitucional o código institucional do usuário
+    * @param password           a senha do usuário
+    * @return o tipo de usuário correspondente ao código e senha fornecidos, ou null se não for encontrado
+    */
+    private String getUserType(int codigoInstitucional, String password) {
+        Path filePath = filesDirectoryPath.resolve("login.txt");
+        try ( BufferedReader reader = new BufferedReader(Files.newBufferedReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split("\\|\\|"); // Dividir a linha usando o separador "||"
+                int codigo = Integer.parseInt(parts[0]);
+                String senha = parts[1];
+                String tipoUsuario = parts[2];
+
+                if (codigo == codigoInstitucional && senha.equals(password)) {
+                    return tipoUsuario; // Encontrou o código e senha correspondentes, retorna o tipo de usuário
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null; // Código e senha não encontrados
     }
 }
