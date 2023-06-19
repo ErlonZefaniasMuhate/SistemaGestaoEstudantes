0b06afd5e1a1d89b56997ace01a213b716e855c3
Erlon Zefanias Muhate <133228881+ErlonZefaniasMuhate@users.noreply.github.com>
2023-06-19 16:42:25 +0200

    Added a menu pitch for the program.

--- SistemaGestaoEstudantes/src/SistemaGestaoEstudantes/Utilitarios/Menu.java
+++ SistemaGestaoEstudantes/src/SistemaGestaoEstudantes/Utilitarios/Menu.java
@@ -4,37 +4,109 @@
  */
 package SistemaGestaoEstudantes.Utilitarios;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.NoSuchFileException;
 
-/**
- *
- * @author Erlon Z. Muhate
- */
 public class Menu {
-
-    /**
-     * @param args the command line arguments
-     */
+    private static BufferedReader reader = new BufferedReader (new InputStreamReader (System.in));
     public static void main(String[] args) {
         try {
-            var userType = new DataManager() {
-            }.login();
+            var userType = new UserDataManager(){}.login();
             switch (userType.getClass().getSimpleName()) {
-                case "Docente" -> {
-                    //adicionar a interacao para docente
-                }
-                case "Estudante" -> {
-                    //adicionar a interacao para estudante
-                }
-                case "Admin" -> {
-                    //adicionar a interacao para admin
-                }
+                case "Docente" -> showDocenteMenu();
+                case "Estudante" -> showEstudanteMenu();
+                case "Admin" -> showAdminMenu();
             }
         } catch (SecurityException e) {
             System.out.println(e.getMessage());
-        }catch(NoSuchFileException e){
+        } catch (NoSuchFileException e) {
             CRUD.primeiroAdmin();
+        } catch (IOException e){
+            
         }
     }
 
+    private static void showDocenteMenu() throws IOException {
+        
+        int option;
+        do {
+            System.out.println("======== MENU DO DOCENTE ========");
+            System.out.println("1. Visualizar informações dos estudantes");
+            System.out.println("2. Lançar notas dos estudantes");
+            System.out.println("3. Sair");
+            System.out.print("Escolha uma opção: ");
+            option = Integer.parseInt(reader.readLine());
+            switch (option) {
+                case 1:
+                    // Lógica para visualizar informações dos estudantes
+                    break;
+                case 2:
+                    // Lógica para lançar notas dos estudantes
+                    break;
+                case 3:
+                    System.out.println("Saindo do sistema...");
+                    break;
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+            }
+        } while (option != 3);
+        reader.close();
+    }
+
+    private static void showEstudanteMenu() throws IOException {
+        
+        int option;
+        do {
+            System.out.println("======== MENU DO ESTUDANTE ========");
+            System.out.println("1. Visualizar notas");
+            System.out.println("2. Visualizar horários de aula");
+            System.out.println("3. Sair");
+            System.out.print("Escolha uma opção: ");
+            option = Integer.parseInt(reader.readLine());
+            switch (option) {
+                case 1:
+                    // Lógica para visualizar notas
+                    break;
+                case 2:
+                    // Lógica para visualizar horários de aula
+                    break;
+                case 3:
+                    System.out.println("Saindo do sistema...");
+                    break;
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+            }
+        } while (option != 3);
+        reader.close();
+    }
+
+    private static void showAdminMenu() throws IOException {
+       
+        int option;
+        do {
+            System.out.println("======== MENU DO ADMIN ========");
+            System.out.println("1. Gerenciar estudantes");
+            System.out.println("2. Gerenciar docentes");
+            System.out.println("3. Sair");
+            System.out.print("Escolha uma opção: ");
+            option = Integer.parseInt(reader.readLine());
+            switch (option) {
+                case 1:
+                    // Lógica para gerenciar estudantes
+                    break;
+                case 2:
+                    // Lógica para gerenciar docentes
+                    break;
+                case 3:
+                    System.out.println("Saindo do sistema...");
+                    break;
+                default:
+                    System.out.println("Opção inválida! Tente novamente.");
+            }
+        } while (option != 3);
+        reader.close();
+    }
 }
+
